36de07232cbeaab74871a182a0b8bc1f6a113154
beatriztota <beatrizttaa@gmail.com>
2024-09-19 21:09:01 -0300

    responsive update

---
+++ js/Nav.js
@@ -0,0 +1,5 @@
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.nav-mobile').classList.toggle('active');
+  });
+
+console.log('JavaScript carregado com sucesso');
